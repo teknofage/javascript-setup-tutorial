9865192db459310428b03f652736003d052f80e7
teknofage <teknofage@gmail.com>
2021-04-08 16:21:14 -0700

    express setup complete with disguised environment variables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello Project.');
+import express from 'express';
+
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+ 
+const app = express();
+ 
+app.use(cors());
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+ 
+app.listen(process.env.PORTO, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
