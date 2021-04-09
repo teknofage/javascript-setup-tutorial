191648c79e1bfa501d7ba48807279ea8b893b7a3
teknofage <teknofage@gmail.com>
2021-04-08 17:16:28 -0700

    setup epress routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import express from 'express';
 
-import 'dotenv/config';
+import 'dotenv/config.js';
 import cors from 'cors';
-import express from 'express';
  
 const app = express();
  
 app.use(cors());
- 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+app.get('/users', (req, res) => {
+  return res.send('GET HTTP method on user resource');
 });
- 
+
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
 app.listen(process.env.PORTO, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
 );
